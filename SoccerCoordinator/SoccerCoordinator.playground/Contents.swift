6f2acf6686444ba8a507ceedb6df3d8b0069aac7
Marcus Klausen <30321578>
2016-06-16 19:26:16 +0200

    Created data model using an array of dictionaries and assigning it to the constant players

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -2,7 +2,31 @@
 
 import UIKit
 
-var str = "Hello, playground"
 
+// create an array of dictionaries holding each players information
 
-// commit this 
+let players = [ // array
+    ["name": "Joe Smith",           "height": 42, "experience": true,   "guardians": "Jim and Jan Smith"], // dictionaries
+    ["name": "Jill Tanner",         "height": 36, "experience": true,   "guardians": "Clara Tanner"],
+    ["name": "Bill Bon",            "height": 43, "experience": true,   "guardians": "Sara and Jenny Bon"],
+    ["name": "Eva Gordon",          "height": 45, "experience": false,  "guardians": "Wendy and Mike Gordon"],
+    ["name": "Matt Gill",           "height": 40, "experience": false,  "guardians": "Charles and Sylvia Gill"],
+    ["name": "Kimmy Stein",         "height": 41, "experience": false,  "guardians": "Bill and Hillary Stein"],
+    ["name": "Sammy Adams",         "height": 45, "experience": false,  "guardians": "Jeff Adams"],
+    ["name": "Karl Saygan",         "height": 42, "experience": true,   "guardians": "Heather Bledsoe"],
+    ["name": "Suzane Greenberg",    "height": 44, "experience": true,   "guardians": "Henrietta Dumas"],
+    ["name": "Sal Dall",            "height": 41, "experience": false,  "guardians": "Gala Dali"],
+    ["name": "Joe Kavalier",        "height": 39, "experience": false,  "guardians": "Sam and Elaine Kavalier"],
+    ["name": "Ben Finkelstein",     "height": 44, "experience": false,  "guardians": "Aaron and Jill Finkelstein"],
+    ["name": "Diego Soto",          "height": 41, "experience": true,   "guardians": "Robin and Sarika Soto"],
+    ["name": "Chloe Alaska",        "height": 47, "experience": false,  "guardians": "David and Jamie Alaska"],
+    ["name": "Arnold Wills",        "height": 43, "experience": false,  "guardians": "Claire Willis"],
+    ["name": "Phillip Helm",        "height": 44, "experience": true,   "guardians": "Thomas Helm and Eva Jones"],
+    ["name": "Les Clay",            "height": 42, "experience": true,   "guardians": "Wynonna Brown"],
+    ["name": "Herschel Krustofski", "height": 45, "experience": true,   "guardians": "Hyman and Rachel Krustofski"]
+]
+
+//
+//      A struct could have been used instead, this way creating a player instance would be easier
+//
+
